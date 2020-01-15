294930e5326df2d10ad82ea93e021f0db84e9774
J. Kim <514kim@gmail.com>
2020-01-15 15:29:55 +0900

    S02P11A316 | S02P11A316-12 create Users model && repository

---
+++ 13_springboot/demo/src/main/java/com/jhee/demo/repository/UsersRepository.java
@@ -0,0 +1,7 @@
+package com.jhee.demo.repository;
+
+import com.jhee.demo.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}
