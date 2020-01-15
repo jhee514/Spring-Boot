6f0960242bcb7160827c1601572503296a5aa057
J. Kim <514kim@gmail.com>
2020-01-15 17:46:18 +0900

    S02P11A316 | S02P11A316-12 add sign up function

--- 13_springboot/demo/src/main/java/com/jhee/demo/Controller/UsersController.java
+++ 13_springboot/demo/src/main/java/com/jhee/demo/Controller/UsersController.java
@@ -1,4 +1,29 @@
 package com.jhee.demo.Controller;
 
+import com.jhee.demo.repository.UsersRepository;
+import com.jhee.demo.service.JoinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class UsersController {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @PostMapping("/joinRequest")
+    public String joinRequest(HttpServletRequest request) {
+        JoinService joinService = new JoinService();
+        joinService.joinUser(request, usersRepository);
+
+        return "index";
+    }
+
+    @PostMapping("/loginRequest")
+    public String loginRequest() {
+        return "index";
+    }
 }
