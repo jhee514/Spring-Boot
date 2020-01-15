5695b6606ea2764c77117b67cb3c7ec5b939a62d
J. Kim <514kim@gmail.com>
2020-01-15 15:28:31 +0900

    S02P11A316 | S02P11A316-12 add MainController.java

---
+++ 13_springboot/demo/src/main/java/com/jhee/demo/Controller/MainController.java
@@ -0,0 +1,21 @@
+package com.jhee.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+//    위아래 같은 의미
+//    @PostMapping
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/join")
+    public String join() {
+        return "join";
+    }
+}
