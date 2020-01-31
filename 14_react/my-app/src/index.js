ae9a5d1c3da369d22d9f96a85dadd24b831a45a5
J. Kim <514kim@gmail.com>
2020-02-01 00:14:48 +0900

    remove App && practice small react

--- 14_react/my-app/src/index.js
+++ 14_react/my-app/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import App from './App';
+/* <App /> */
+
+
+const name = 'Jee'
+const element = <h1>Hello, { name }</h1>
+
+ReactDOM.render(
+    
+    element, 
+    document.getElementById('root')
+    
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
